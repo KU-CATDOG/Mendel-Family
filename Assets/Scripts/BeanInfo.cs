a6dff41f5223aee1a8b505ceaa9c918588728d1f
16홍선우 <hsu97cow@naver.com>
2020-07-23 00:16:16 +0900

    Bean slots added
    Temporary images added

--- Assets/Scripts/BeanInfo.cs
+++ Assets/Scripts/BeanInfo.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class BeanInfo : MonoBehaviour
 {
-    int y;
-    int _Y;
+    public bool fixedBean;
+
+    // Genotype variables in arrays. Dominant gene comes first.
+
+    // Bean color
+    public int[] y = new int[2];
+
+    // Bean shape
+    public int[] r = new int[2];
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Bean")
+            Physics.IgnoreCollision(col.collider, this.GetComponent<Collider>());
+    }
 }
---
+++ Assets/Scripts/SlotController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotController : MonoBehaviour
+{
+    public bool parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Bean")
+        {
+            other.transform.position = new Vector3(transform.position.x, transform.position.y, other.transform.position.z);
+        }
+    }
+}
