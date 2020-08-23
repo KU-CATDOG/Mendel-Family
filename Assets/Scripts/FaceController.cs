d508daa5ccc401ce0e845435d0abdfdb09e588ee
16홍선우 <hsu97cow@naver.com>
2020-08-23 19:18:07 +0900

    Bean face added
    Need to add more animation

---
+++ Assets/Scripts/FaceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceController : MonoBehaviour
+{
+    GameObject faceOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        faceOn = this.gameObject.transform.GetChild(UnityEngine.Random.Range(0, 10)).gameObject;
+        faceOn.SetActive(true);
+
+        if (UnityEngine.Random.Range(0, 2) == 0)
+        faceOn.GetComponent<SpriteRenderer>().flipX = true;
+    }
+}
