3373b2748829751245095c29c54397dce90151ba
원하진 <wonhj12@naver.com>
2020-08-25 16:49:50 +0900

    Save before deleting unnecessary files

--- Assets/Scripts/BeanController.cs
+++ Assets/Scripts/BeanController.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class BeanController : MonoBehaviour
 {
+    LevelManager manager;
     public Material[] beanMaterial;
     public Sprite[] beanSprite;
     BeanInfo beanInfo;
 
     void Start()
     {
+        manager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
         beanInfo = GetComponent<BeanInfo>();
 
         int ySum = beanInfo.Y[0] + beanInfo.Y[1];
         int rSum = beanInfo.R[0] + beanInfo.R[1];
-        int index = 0; // 0 - YYRR, 1 - yyRR, 2 - YYrr, 3 - yyrr; YY and RR includes Yy and Rr
+        int rwSum = beanInfo.RW[0] + beanInfo.RW[1];
+        int index = 0;
 
         if (ySum == 2)   // If yy
         {
@@ -24,6 +27,28 @@ public class BeanController : MonoBehaviour
         {
             index += 2;
         }
+        /*
+        if (rSum == 2)  // if rr
+        {
+            index += 5;
+        }
+        if (rwSum == 0)
+        {
+            if (manager.csvFile == "Stage2")
+            {
+                index += 2;
+            }
+        }
+        else if (rwSum == 1)
+        {
+            index += 3;
+        }
+        else if (rwSum == 2)
+        {
+            index += 4;
+        }
+        */
+
 
         gameObject.GetComponent<SpriteRenderer>().material = beanMaterial[index];
         gameObject.GetComponent<SpriteRenderer>().sprite = beanSprite[index];
